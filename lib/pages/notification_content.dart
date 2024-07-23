e24f4e9f213aad93d250e2dfd47f5204943f242a
brwla <95474989+brwla@users.noreply.github.com>
2024-07-23 11:37:22 +0000

    Modified Notifications
    - Centered the Notification Title.
    - Changed the color of the Notification Content.

--- lib/pages/notification_content.dart
+++ lib/pages/notification_content.dart
@@ -1,61 +1,61 @@
 import 'package:flutter/material.dart';
 
-class NotificationContent extends StatefulWidget {
+class NotificationContent extends StatelessWidget {
   final String title;
   final String content;
   final IconData icon;
   final Color color;
-  const NotificationContent(
-      {super.key,
-      required this.title,
-      required this.content,
-      required this.icon,
-      required this.color});
 
-  @override
-  State<NotificationContent> createState() => _NotificationContentState();
-}
+  const NotificationContent({
+    Key? key,
+    required this.title,
+    required this.content,
+    required this.icon,
+    required this.color,
+  }) : super(key: key);
 
-class _NotificationContentState extends State<NotificationContent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 6),
-      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Theme.of(context).colorScheme.onPrimaryContainer,
         boxShadow: [
           BoxShadow(
-            color: Theme.of(context).colorScheme.shadow.withOpacity(0.5),
-            spreadRadius: 1,
-            blurRadius: 3,
-            offset: const Offset(0, 1),
+            color: Theme.of(context).colorScheme.shadow.withOpacity(0.3),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: const Offset(2.5, 5),
           ),
         ],
       ),
       child: Row(
         children: [
           Icon(
-            widget.icon,
-            color: widget.color,
+            icon,
+            color: color,
             size: 50,
           ),
-          const SizedBox(width: 10,),
+          const SizedBox(
+            width: 10,
+          ),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  widget.title,
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.secondary,
-                      fontSize: 20),
-                ),
-                Text(
-                  widget.content,
+                  title,
                   style: TextStyle(
                     color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  content,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onSecondaryContainer,
                   ),
                 )
               ],
